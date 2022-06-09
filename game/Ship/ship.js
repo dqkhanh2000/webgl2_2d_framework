75c3438d4c1e3ee214a540a25bf0ea591ee4343a
Khanh Duong <khanh.duong@pureative.com>
2022-06-09 18:33:23 +0700

    setup animated ship

--- game/Ship/ship.js
+++ game/Ship/ship.js
@@ -1,17 +1,23 @@
+import { AnimatedSprite } from "../../src/core/AnimatedSprite";
 import { Sprite } from "../../src/core/Sprite";
+import { TextureCache } from "../../src/core/Texture";
 
 export const ShipEvent = Object.freeze({
   TakeDamage: "take-damage",
 });
-export class Ship extends Sprite {
-  constructor(gl, textureShip) {
-    super(gl, textureShip);
+export class Ship extends AnimatedSprite {
+  constructor(gl) {
+    let textures = [];
+    for (let i = 1; i <= 16; i++) {
+      textures.push(TextureCache.get(`./dist/images/ship/ship_${i}.png`));
+    }
+    super(gl, textures, { duration: 0.2, loop: true, autoPlay: true });
     this.gl = gl;
     this.listEnemy = [];
 
-    this.textureShip = textureShip;
     this.transform.position.x = 100;
     this.transform.position.y = 100;
+    this.transform.scale.set(1, 1.5);
     this.transform.rotation = -Math.PI / 2;
 
   }
--- game/game.js
+++ game/game.js
@@ -1,7 +1,7 @@
 import { TextureCache } from "../src/core/Texture";
 import Ticker from "../src/system/ticker";
 import Engine2D from "../src/core/Engine";
-import Particle from "../src/renderers/Particle";
+import Particle from "../src/core/Particle";
 import Loader from "../src/core/Loader";
 import { Ship, ShipEvent } from "./Ship/ship";
 import { EnemyManager } from "./Enemy/enemyManager";
@@ -17,15 +17,28 @@ export class MyGame {
     this.delaySpawn = true;
     this.checkLevel = 1;
     this.load();
-    this.particle = new Particle(0, 0.5, 1000, 0.1, 0.5, 1, -Math.PI, Math.PI, 0.5, 1, [0.0, -0.8]);
-    this.core.stage.addChild(this.particle);
     Ticker.SharedTicker.add(() => {
       this.core.update();
     });
   }
 
   load() {
+    Loader.addSrc("./dist/images/ship/ship_1.png");
+    Loader.addSrc("./dist/images/ship/ship_2.png");
+    Loader.addSrc("./dist/images/ship/ship_3.png");
+    Loader.addSrc("./dist/images/ship/ship_4.png");
+    Loader.addSrc("./dist/images/ship/ship_5.png");
+    Loader.addSrc("./dist/images/ship/ship_6.png");
+    Loader.addSrc("./dist/images/ship/ship_7.png");
+    Loader.addSrc("./dist/images/ship/ship_8.png");
     Loader.addSrc("./dist/images/ship/ship_9.png");
+    Loader.addSrc("./dist/images/ship/ship_10.png");
+    Loader.addSrc("./dist/images/ship/ship_11.png");
+    Loader.addSrc("./dist/images/ship/ship_12.png");
+    Loader.addSrc("./dist/images/ship/ship_13.png");
+    Loader.addSrc("./dist/images/ship/ship_14.png");
+    Loader.addSrc("./dist/images/ship/ship_15.png");
+    Loader.addSrc("./dist/images/ship/ship_16.png");
     Loader.addSrc("./dist/images/sad.png");
     Loader.addSrc("./dist/images/redBullet.png");
     Loader.addSrc("./dist/images/enemy/enemy_1.png");
@@ -50,8 +63,7 @@ export class MyGame {
   }
 
   spawnShip() {
-    let textureShip = TextureCache.get("./dist/images/ship/ship_9.png");
-    this.ship = new Ship(this.core.gl, textureShip);
+    this.ship = new Ship(this.core.gl);
     this.ship.on(ShipEvent.TakeDamage, this.ship.takeDamage, this.ship);
 
     let textureBullet = TextureCache.get("./dist/images/redBullet.png");

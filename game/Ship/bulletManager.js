c2add56bdb70a77f4af496950025bc4e7659ae1c
phuong <phuong812000.bq@gmail.com>
2022-06-09 16:11:17 +0700

    - update bullet manager

--- game/Ship/bulletManager.js
+++ game/Ship/bulletManager.js
@@ -1,4 +1,5 @@
 import Container from "../../src/core/Container";
+import { Sprite } from "../../src/core/Sprite";
 import Ticker from "../../src/system/ticker";
 import { Bullet } from "./bullet";
 
@@ -17,21 +18,24 @@ export class BulletManager extends Container {
   }
 
   spawnBullet() {
-    let bullet = new Bullet(this.gl, this.textureBullet);
+    for (let i = 0; i < this.checkLevel; i++) {
+      if (this.checkLevel === 1) {
+        let bullet = new Bullet(this.gl, this.textureBullet);
 
-    this.addChild(bullet);
-    bullet.setPosition(this.posX, this.posY);
-    this.listBullet.push(bullet);
-
-    Ticker.SharedTicker.add((dt) => {
-      if (!bullet._destroyed) {
-        if (bullet.transform.position.y < 10) {
-          bullet.destroy();
-        }
-        else {
-          bullet.transform.position.y -= dt * 1000;
-        }
+        this.addChild(bullet);
+        bullet.setPosition(this.posX, this.posY);
+        this.listBullet.push(bullet);
       }
+      else if (this.checkLevel === 2) {
+        let bullet = new Bullet(this.gl, this.textureBullet);
+        this.addChild(bullet);
+        bullet.setPosition(this.posX - 10 + 20 * i, this.posY - 10 + 20 * i);
+        this.listBullet.push(bullet);
+      }
+    }
+    this.listBullet.forEach((bullet) => {
+      bullet.updatePosition();
     });
+    this.listBullet = [];
   }
 }
